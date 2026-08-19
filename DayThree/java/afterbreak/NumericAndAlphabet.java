9c8a2e986b4d65c09e739aef8601d63cc7a93f15
Annamero-code <yakuburasheedannamero@gmail.com>
2026-08-19 17:09:37 +0100

    push

---
+++ DayThree/java/afterbreak/NumericAndAlphabet.java
@@ -0,0 +1,25 @@
+public class NumericAndAlphabet {
+    
+    public static String integerAndAlphabet (String word){
+        String word = "a111BAcb";
+        for(int counter = 1; counter < word.length; counter ++){
+            for(int index = 1; index < counter.length ; index ++){
+                if(counter charAt[0] == index charAt[1] ){
+                
+                }
+                
+                return 
+            } 
+        }
+    
+    }
+
+
+    public static void main(String [] args){
+    
+    }
+    
+    
+    
+    
+}    
